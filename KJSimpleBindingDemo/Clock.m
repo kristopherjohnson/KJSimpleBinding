1a1b550e2aaecb04fc7e3d2b68977939a0549c2a
Kristopher Johnson <kris@kristopherjohnson.net>
2012-03-22 16:39:14 -0400

    Update screen every tenth of a second

--- KJSimpleBindingDemo/Clock.m
+++ KJSimpleBindingDemo/Clock.m
@@ -62,7 +62,7 @@
     
     // Set up timer to do this again
     if (_isStarted) {
-        double delayInSeconds = 1.0;
+        double delayInSeconds = 0.1;
         dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
         dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
             [self tick];
