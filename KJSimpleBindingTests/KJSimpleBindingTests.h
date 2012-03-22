ebcc3424e4d1bc948696fb3bebe8d001776eb9fd
Kristopher Johnson <kris@kristopherjohnson.net>
2012-03-21 20:18:57 -0400

    Initial Commit

---
+++ KJSimpleBindingTests/KJSimpleBindingTests.h
@@ -0,0 +1,13 @@
+//
+//  KJSimpleBindingTests.h
+//  KJSimpleBindingTests
+//
+//  Created by Kristopher Johnson on 3/21/12.
+//  Copyright (c) 2012 Capable Hands Technologies, Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KJSimpleBindingTests : SenTestCase
+
+@end
---
+++ KJSimpleBindingTests/KJSimpleBindingTests.m
@@ -0,0 +1,32 @@
+//
+//  KJSimpleBindingTests.m
+//  KJSimpleBindingTests
+//
+//  Created by Kristopher Johnson on 3/21/12.
+//  Copyright (c) 2012 Capable Hands Technologies, Inc. All rights reserved.
+//
+
+#import "KJSimpleBindingTests.h"
+
+@implementation KJSimpleBindingTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in KJSimpleBindingTests");
+}
+
+@end
